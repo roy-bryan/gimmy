e73fda090cea4ac2c76f7acb1d6ef73c04092fd3
roy-bryan <109702917+roy-bryan@users.noreply.github.com>
2022-07-26 05:34:43 -0500

    Add length bit, make gimmy() function and fix indentation issues

--- gimmy.cpp
+++ gimmy.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+unsigned int gimmy(unsigned int, int);
+
 int numberOfBitsUsed(unsigned int);
 unsigned int firstUsedBits(unsigned int, int);
 unsigned int lastBits(unsigned int, int);
@@ -14,45 +16,46 @@ unsigned int lastDiv(unsigned int p, unsigned int b, int length);
 unsigned int lastProd(unsigned int a, unsigned int b, int length);
 
 int main(int argc, char *argv[]) {
-   unsigned int inputNumber = atoi(argv[1]);
-   int targetLength = atoi(argv[2]);
-   
-   if(targetLength == 0){ //If no second argument is given, or if it is zero, hash to a 32-bit value
-   	 targetLength = 32;
-   }
-   
-   int length = numberOfBitsUsed(inputNumber); //Number of bits in input number
-   
-   int halfLength = (length + 1)/2; //Number of bits in each half (for integers with an odd number of bits, the middle bit is shared by both halves)
-   
-   //Separate number into two halves
-   unsigned int firstHalf = firstUsedBits(inputNumber, halfLength); //Used to generate b
-   unsigned int lastHalf = lastBits(inputNumber, halfLength); //Used to generate p
-   
-   //Create b and p
-   unsigned int b = generatePart(firstHalf, halfLength, targetLength);
-   unsigned int p = generatePart(lastHalf, halfLength, targetLength);
-   
-   //Make sure b and p are not equal because that makes for a boring hash value
-   if(p == b){
-   	b = b ^ -1;
-   }
-   
-   unsigned int a = lastDiv(p, b, targetLength); //Hash value
-   
-   cout << a << endl; //Output result
-   
-   return 0;
+	unsigned int inputNumber = atoi(argv[1]);
+	int targetLength = atoi(argv[2]);
+	
+	if(targetLength == 0){ //If no second argument is given, or if it is zero, hash to a 32-bit value
+		targetLength = 32;
+	}
+	cout << gimmy(inputNumber, targetLength) << endl;
+	
+	return 0;
+}
+
+unsigned int gimmy(unsigned int inputNumber, int targetLength){
+	int length = numberOfBitsUsed(inputNumber); //Number of bits in input number
+	int lengthBitP = length & 1; //If the length is odd, set p's length bit to 1 and vise versa
+	lengthBitP = lengthBitP << 1; //The length bit will be the second to last bit, so it should be shifted left
+	int lengthBitB = lengthBitP ^ 2; //b's length bit will be the opposite of p's. We can get this by XORing with ...00010 (2)
+	
+	int halfLength = (length + 1)/2; //Number of bits in each half (for integers with an odd number of bits, the middle bit is shared by both halves)
+	
+	//Separate number into two halves
+	unsigned int firstHalf = firstUsedBits(inputNumber, halfLength); //Used to generate b
+	unsigned int lastHalf = lastBits(inputNumber, halfLength); //Used to generate p
+	
+	//Create b and p
+	unsigned int b = generatePart(firstHalf, halfLength, targetLength) + lengthBitB;
+	unsigned int p = generatePart(lastHalf, halfLength, targetLength) + lengthBitP;
+	
+	unsigned int a = lastDiv(p, b, targetLength); //Hash value
+	
+	return a; //Return result
 }
 
 int numberOfBitsUsed(unsigned int num){ //Takes a 32-bit number and returns the number of bits that would remain if all leading zeros were removed
-   int length = 0; //Initialize length to zero
-   
-   while (num >= pow(2, length)) { //For each power of two that the number is greater than, increment length by 1
-   	length++;
-   }
-   
-   return length;
+	int length = 0; //Initialize length to zero
+	
+	while (num >= pow(2, length)) { //For each power of two that the number is greater than, increment length by 1
+		length++;
+	}
+	
+	return length;
 }
 
 unsigned int firstUsedBits(unsigned int x, int length){ //Returns a number with only the first n bits of x (not counting leading zeros)
@@ -69,34 +72,36 @@ unsigned int lastBits(unsigned int x, int n){ //Returns a number with only the l
 	return x % (unsigned int)pow(2,n); //Remove unwanted bits by taking num modulus a power of two
 }
 
-unsigned int generatePart(unsigned int x, int xLength, int targetLength){ //Generates either b or p from x  
-   unsigned int part; //Value to be returned
-   
-   //Handle edge cases with default values
-   if(xLength > 32 || xLength < 0) xLength = 32;
-   if(targetLength > 32 || targetLength < 0) targetLength = 32;
-   
-   unsigned int temp = x; //Copy x so we can mutilate it
-   int parity = 0; //Parity bit to make an even number of 1's in x
-   
-   for (int i = 0; i < xLength; i++) { //Iterate through x
-   	 int bit = temp & 1; //Current bit
-	 parity = parity xor bit; //Flip parity bit if the current bit is a one
-	 temp = temp >> 1; //Shift temp to get ready for the next bit
-   }
-   
-   
-   if(targetLength <= xLength) { //If the desired length is less than or equal to the length of x, shift the number to only have the desired number of bits
-     part = x >> (xLength - targetLength);
-   }
-   else { //If the desired length is greater than the length of x, shift to the left to make room for the parity bit
-   	 part = x << 1;
-   }
-   part = part & -2; //Zero the last bit to make room for the parity bit
-   
-   part = part + parity; //Add the parity bit to the end
-   
-   return part; //Return the number
+unsigned int generatePart(unsigned int x, int xLength, int targetLength){ //Generates either b or p from x, leaving the second to last bit blank (0)
+	unsigned int part; //Value to be returned
+	
+	//Handle edge cases with default values
+	if(xLength > 32 || xLength < 0) xLength = 32;
+	if(targetLength > 32 || targetLength < 0) targetLength = 32;
+	
+	unsigned int temp = x; //Copy x so we can mutilate it
+	int parity = 0; //Parity bit to make an even number of 1's in x
+	
+	for (int i = 0; i < xLength; i++) { //Iterate through x
+		int bit = temp & 1; //Current bit
+		parity = parity xor bit; //Flip parity bit if the current bit is a one
+		temp = temp >> 1; //Shift temp to get ready for the next bit
+	}
+	
+	if(targetLength <= xLength) { //If the desired length is less than or equal to the length of x, shift the number to only have the desired number of bits
+		part = x >> (xLength - targetLength);
+	}
+	else if(targetLength = xLength+1) { //If the desired length is greater than the length of x by one bit, shift to the left by one to achieve desired length
+		part = x << 1;
+	}
+	else {
+		part = x << 2; //If the desired length is greater than the length of x by more than one bit, shift to the left to make room for the length bit and the parity bit
+	}
+	part = part & -4; //Zero the last 2 bits to make room for the length bit and parity bit
+	
+	part = part + parity; //Add the parity bit to the end
+	
+	return part; //Return the number
 }
 
 unsigned int lastDiv(unsigned int p, unsigned int b, int length){ //Returns the last few bits of the number, a, which multiplies with b to get a product with the same last few digits of the number p
@@ -113,6 +118,7 @@ unsigned int lastDiv(unsigned int p, unsigned int b, int length){ //Returns the 
 unsigned int lastProd(unsigned int a, unsigned int b, int length){ //Appends a 1 to the end of each number, then multiplies them and returns the product shifted right
 	unsigned long divA = (a << 1) + 1; //Append 1 to the end of a
 	unsigned long divB = (b << 1) + 1; //Append 1 to the end of b
+	
 	unsigned long product = divA*divB; //Multiply a and b to find the product
 	
 	product = product >> 1; //Remove the last bit from the product
